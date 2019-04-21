9d50a98ef76a73940f3114804192db302a1783a5
BigMike783 <45283126+BigMike783@users.noreply.github.com>
2019-04-21 14:04:42 -0600

    answered questions

--- spread.js
+++ spread.js
@@ -18,7 +18,7 @@ let product = {
 }
 
 // code here
-
+let productCopy = {...product}
 
 
 
@@ -29,7 +29,7 @@ let product = {
 let colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
 
 // code here
-
+let colorsCopy = {...colors}
 
 
 
@@ -44,8 +44,8 @@ function addNums(num1, num2, num3) {
 }
 
 // code here
-
-
+let result = addNums(...numbers)
+console.log(result)
 ////////// PROBLEM 4 //////////
 
 /* Using the spread operator, concatentate the two arrays below and save it to a variable named "combinedArrays" */
@@ -54,3 +54,4 @@ let arr1 = ['dog', 'chameleon', 'monkey'];
 let arr2 = ['bear', 'tiger', 'liger'];
 
 // code here
+let combinedArrays = [...arr1,...arr2]
